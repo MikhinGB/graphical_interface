2d8efa9fbe01f5d001a8b136786d9d6b6ff3772f
МГБ <nd_mihin@mail.ru>
2024-11-08 20:06:00 +0400

    081124_1

--- drawing_app.py
+++ drawing_app.py
@@ -4,24 +4,29 @@ from PIL import Image, ImageDraw
 
 
 class DrawingApp:
+
     def __init__(self, root):
+
+        self.clicks = 0
+
         self.root = root
         self.root.title("Рисовалка с сохранением в PNG")
 
         self.image = Image.new("RGB", (600, 400), "white")
         self.draw = ImageDraw.Draw(self.image)
 
-        self.canvas = tk.Canvas(root, width=600, height=400, bg='white')
+        self.canvas_color = 'white'
+        self.canvas = tk.Canvas(root, width=600, height=400, bg=self.canvas_color)
         self.canvas.pack()
 
         self.setup_ui()
 
         self.last_x, self.last_y = None, None
-        self.pen_color = 'black'
+        self.pen_color_in = 'black'
+        self.pen_color = self.pen_color_in
 
         self.canvas.bind('<B1-Motion>', self.paint)
         self.canvas.bind('<ButtonRelease-1>', self.reset)
-
 
     def setup_ui(self):
         control_frame = tk.Frame(self.root)
@@ -36,18 +41,18 @@ class DrawingApp:
         save_button = tk.Button(control_frame, text="Сохранить", command=self.save_image)
         save_button.pack(side=tk.LEFT)
 
+        self.eraser_button = tk.Button(control_frame, text="Ластик", command=self.click_button)
+        self.eraser_button.pack(side=tk.RIGHT)
+
         self.brush_size_scale = tk.Scale(control_frame, from_=1, to=10, orient=tk.HORIZONTAL)
         self.brush_size_scale.pack(side=tk.LEFT)
 
         sizes = [1, 2, 5, 10]
 
-        self.brush_size_menu = tk.OptionMenu(control_frame,
+        self.brush_size_menu = OptionMenu(control_frame,
                                              self.brush_size_scale,
                                              *sizes)
         self.brush_size_menu.pack(side=tk.LEFT)
-        # variable.set(2)
-        #
-        # self.variable = variable.get(); print(self.variable)
 
     def paint(self, event):
         if self.last_x and self.last_y:
@@ -60,6 +65,14 @@ class DrawingApp:
         self.last_x = event.x
         self.last_y = event.y
 
+
+    def click_button(self):
+        self.clicks += 1
+        if self.clicks % 2 == 0:
+            self.pen_color = self.pen_color_in
+        else:
+            self.pen_color = self.canvas_color
+
     def reset(self, event):
         self.last_x, self.last_y = None, None
 
@@ -69,7 +82,7 @@ class DrawingApp:
         self.draw = ImageDraw.Draw(self.image)
 
     def choose_color(self):
-        self.pen_color = colorchooser.askcolor(color=self.pen_color)[1]
+        self.pen_color = colorchooser.askcolor(color=self.pen_color_in)[1]
 
     def save_image(self):
         file_path = filedialog.asksaveasfilename(filetypes=[('PNG files', '*.png')])
